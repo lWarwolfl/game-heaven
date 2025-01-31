3e6bf4e167ecfec1081c3d23d4e8f266f161e06f
Sina Kheiry <sinakheiri.dev@gmail.com>
2025-01-31 17:47:13 +0330

    fix: github name bug step 2

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import AnimatedButton from '@/components/ui/AnimatedButton/AnimatedButton'
-import Button from '@/components/ui/Button2/Button'
+import Button from '@/components/ui/Button/Button'
 
 export default function Home() {
   return (
